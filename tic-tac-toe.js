61f110ef4d549aa7ee93c408589a200da0d16cd9
IshshahBrooks <ishshah1brooks16@gmail.com>
2022-10-14 17:57:29 -0500

    "Check for the winner and update the status"

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -24,15 +24,63 @@ onload = function() {
               document.getElementById('board').getElementsByTagName('div')[i].className+=('X');
           }
 
-          track_state.push(gamesquares[i].innerHTML = current);
+          track_state.push(game_square[i].innerHTML = current);
           console.log(track_state)
       
         game_square[i].addEventListener("mouseover", event => {
-            game_square[i].className+=(' hover');
+        game_square[i].className+=(' hover');
         });
         game_square[i].addEventListener("mouseleave", event => {
-            game_square[i].classList.remove('hover');
+        game_square[i].classList.remove('hover');
        });
+         
+          check_Winner();
+          function check_Winner(){
+            let box0 = game_square[0].innerHTML;
+            let box1 = game_square[1].innerHTML;
+            let box2 = game_square[2].innerHTML;
+            let box3 = game_square[3].innerHTML;
+            let box4 = game_square[4].innerHTML;
+            let box5 = game_square[5].innerHTML;
+            let box6 = game_square[6].innerHTML;
+            let box7 = game_square[7].innerHTML;
+            let box8 = game_square[8].innerHTML;
+            
+            if ( box0 != "" && box0 == box1 && box0 == box2){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+            if ( box3 == box4 && box3==box5 && box3!= ""){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+            if ( box6 == box7 && box6==box8 && box6 != ""){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+            if ( box0 == box4 && box0==box8 && box4 != ""){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+            if ( box2 == box4 && box2==box6 && box2 != ""){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+            if ( box0 == box3 && box0==box6 && box0 != ""){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+            if ( box1 == box4 && box1==box7 && box7 != ""){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+            if ( box2 == box5 && box2==box8 && box2 != ""){
+              game_stat.textContent= "Congratulations! " + track_state[track_state.length-1] +"is the Winner!" ;
+              game_stat.className+="you-won";
+            }
+          }
+          
+      
     }
     }
       

49bfc81fb9e87e6f32ddb06f94026b2bfd16b0d3
IshshahBrooks <ishshah1brooks16@gmail.com>
2022-10-14 11:22:29 -0500

    "Add an X or O to a square when clicked"

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,32 @@
 onload = function() {
-  let chess = document.getElementsById("board").children.length;
-  let i=0;
-  while ( i <= chess){
-    document.getElementById('board').getElementsByTagName("div")[i].className+='square';
-    i++;
-    }
-  }
+  let game_x = 'X';
+  let game_o = 'O';
+  var track_state = [];
 
+  let game_square = document.getElementsById("board").getElementsByTagName("div");
+  let game_stat = document.getElementsById("status");
+
+  for ( i = 0; i < game_square.lenght; i++ ){
+    game_square[i].className+=('square');
+    game_square[i].addEventListener("click", playclick, { once;true});
+    }
+  var game_player;
+  function playclick(){
+    track_state.push(game_x);
+    console.log(track_state);
+    console.log('Clicked')
+    if (game_player == game_x){
+        game_player = game_o;
+        document.getElementById('board').getElementsByTagName('div')[i].className+=('X');
+    }
+    else{
+      game_player= game_x;
+      document.getElementById('board').getElementsByTagName('div')[i].className+=('O');
+    }
+
+    track_state.push(gamesquares[i].innerHTML = game_player);
+      console.log(track_state)
+    }
+   
+  }
 
